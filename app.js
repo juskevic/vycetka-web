6d571ecc8e1a84527e6d03dbcf67da3baf59f32f
makuyoshi <73663894+makuyoshi@users.noreply.github.com>
2024-04-18 19:56:36 +0200

    Added green text styling when reaching grand total of 8000

--- app.js
+++ app.js
@@ -11,29 +11,55 @@ function calculateTotals() {
         let total = 0;
 
         switch (id) {
-            case 'count2000': total = value * 2000; break;
-            case 'count1000': total = value * 1000; break;
-            case 'count500': total = value * 500; break;
-            case 'count200': total = value * 200; break;
-            case 'count100': total = value * 100; break;
-            case 'count50': total = value * 50; break;
-            case 'count20': total = value * 20; break;
-            case 'count10': total = value * 10; break;
-            case 'count5': total = value * 5; break;
-            case 'count2': total = value * 2; break;
-            case 'count1': total = value * 1; break;
-            case 'countEuro': total = value * 22; break;
+            case 'count2000':
+                total = value * 2000;
+                break;
+            case 'count1000':
+                total = value * 1000;
+                break;
+            case 'count500':
+                total = value * 500;
+                break;
+            case 'count200':
+                total = value * 200;
+                break;
+            case 'count100':
+                total = value * 100;
+                break;
+            case 'count50':
+                total = value * 50;
+                break;
+            case 'count20':
+                total = value * 20;
+                break;
+            case 'count10':
+                total = value * 10;
+                break;
+            case 'count5':
+                total = value * 5;
+                break;
+            case 'count2':
+                total = value * 2;
+                break;
+            case 'count1':
+                total = value * 1;
+                break;
+            case 'countEuro':
+                total = value * 22;
+                break;
         }
 
         totalCell.textContent = `${total} Kč`;
         grandTotal += total;
     });
-    totalAll.textContent = `${grandTotal} Kč`;
     if (grandTotal === 8000) {
-        totalAll.innerHTML = '<td class="col-4 fw-bold" id="totalAll" style="color: green;"></td>';
-        totalAll.textContent = `${grandTotal} Kč`;
+        totalAll.style.color = 'green';
+    } else {
+        totalAll.style.color = '';
     }
+    totalAll.textContent = `${grandTotal} Kč`;
 }
+
 inputs.forEach(input => {
     input.addEventListener('input', calculateTotals);
 });
