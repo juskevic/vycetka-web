3b440761ff178251f55994d25c5d29cb1aebd412
juskevic <73663894+juskevic@users.noreply.github.com.>
2024-08-06 17:55:06 +0200

    Add app store badges to home page
    This commit includes the addition of Google Play and App Store badges to the home page, enhancing visibility and providing direct download links. The new badges are positioned using a flex container, ensuring proper alignment across various screen sizes.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,22 @@
 "use client";
 import React from "react";
 import {SparklesCore} from "@/components/ui/sparkles";
+import Image from "next/image";
 
 export default function Home() {
   return (
       <>
         <div className="h-screen relative w-full bg-black flex flex-col items-center justify-center overflow-hidden rounded-md">
-          <div className="w-full absolute inset-0 h-screen">
-            <SparklesCore
-                id="tsparticlesfullpage"
-                background="transparent"
-                minSize={0.6}
-                maxSize={1.4}
-                particleDensity={100}
-                className="w-full h-full"
-                particleColor="#FFFFFF"
-            />
-          </div>
-          <h1 className="text-2xl md:text-5xl xl:text-7xl font-bold text-center text-white relative z-20">
-            Výčetka
-          </h1>
+            <div className="w-full absolute inset-0 h-screen">
+                <SparklesCore id="tsparticlesfullpage" background="transparent" minSize={0.6} maxSize={1.4} particleDensity={100} className="w-full h-full" particleColor="#FFFFFF"/>
+            </div>
+            <h1 className="text-2xl md:text-5xl xl:text-7xl font-bold text-center text-white relative z-20">
+                Výčetka
+            </h1>
+            <div className="z-10 flex flex-col md:flex-row gap-10 pt-12">
+                <Image width={200} height={200} alt="get it on google play" src="/googleplay.png"></Image>
+                <Image width={200} height={200} alt="get it on google play" src="/appstore.png"></Image>
+            </div>
         </div>
       </>
   );
