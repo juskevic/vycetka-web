7c23cd7d524d1d30a2f417686bb7ca8a14ffadcb
juskevic <73663894+juskevic@users.noreply.github.com.>
2024-08-06 17:36:50 +0200

    Add custom font and improve text visibility
    Applied the 'DigitalCards' font to enhance the visual theme. Removed unused imports and adjusted animation to await completion in sparkles component. Refined homepage layout with improved text responsiveness.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,7 @@ import {SparklesCore} from "@/components/ui/sparkles";
 export default function Home() {
   return (
       <>
-        <div
-            className="h-screen relative w-full bg-black flex flex-col items-center justify-center overflow-hidden rounded-md">
+        <div className="h-screen relative w-full bg-black flex flex-col items-center justify-center overflow-hidden rounded-md">
           <div className="w-full absolute inset-0 h-screen">
             <SparklesCore
                 id="tsparticlesfullpage"
@@ -18,7 +17,7 @@ export default function Home() {
                 particleColor="#FFFFFF"
             />
           </div>
-          <h1 className="md:text-7xl text-3xl lg:text-6xl font-bold text-center text-white relative z-20">
+          <h1 className="text-4xl md:text-6xl xl:text-8xl font-bold text-center text-white relative z-20">
             Výčetka
           </h1>
         </div>
