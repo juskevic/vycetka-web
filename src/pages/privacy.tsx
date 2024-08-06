b050f168d31fa02521d367ff64212c1283dc27a4
juskevic <73663894+juskevic@users.noreply.github.com.>
2024-08-06 21:21:20 +0200

    Update privacy link and set global background color
    Changed the "Go back" link in the privacy page to redirect to the homepage instead. Additionally, set the global background color of the app to black to enhance visual consistency.

--- src/pages/privacy.tsx
+++ src/pages/privacy.tsx
@@ -15,7 +15,7 @@ export default function PrivacyPage() {
                 <div className="flex justify-center max-w-2xl mx-auto p-10 pt-20 text-gray-200">
                     <div className="flex flex-col gap-7 flex-wrap">
                         <div className="flex justify-end text-gray-200">
-                            <Link href="/privacy">
+                            <Link href="/">
                                 <button className="shadow-[0_0_0_3px_#000000_inset] px-6 py-2 bg-transparent border border-black dark:border-gray-300 dark:text-white text-black rounded-full font-bold transform hover:-translate-y-1 transition duration-400">
                                     Zpátky / Go back
                                 </button>
