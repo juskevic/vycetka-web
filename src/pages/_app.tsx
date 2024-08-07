13d1bec9c9a1dd579f261bbae2fa6f27e12454cf
juskevic <73663894+juskevic@users.noreply.github.com.>
2024-08-07 13:23:59 +0200

    Update app metadata with new title and Open Graph tags
    Changed the title to "Výčetka by Max Juškevič" and added a favicon link. Included Open Graph tags for better social media integration, specifying title, image, URL, and site name.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,8 +6,13 @@ function MyApp({ Component, pageProps }: AppProps) {
     return (
         <>
             <Head>
-                <title>My App</title>
-                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                <title>Výčetka by Max Juškevič</title>
+                <link rel="icon" type="image/png" href="/favicon.ico"/>
+                <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+                <meta property="og:title" content="Výčetka by Max Juškevič"/>
+                <meta property="og:image" content="/GraphicFeature.png"/>
+                <meta property="og:url" content="URL of your website"/>
+                <meta property="og:site_name" content="Výčetka by Max Juškevič"/>
             </Head>
             <Component {...pageProps} />
         </>
