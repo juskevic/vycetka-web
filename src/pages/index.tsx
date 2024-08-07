e225a169471400db5b8c9a5ec3e9086586699b47
juskevic <73663894+juskevic@users.noreply.github.com.>
2024-08-07 13:05:21 +0200

    Update homepage and footer styles
    Adjusted homepage div height for responsiveness and wrapped an image with a link. Also, modified the footer to be more flexible across devices and revised the privacy link style.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,7 @@ import Link from "next/link";
 const Home = () => {
   return (
       <>
-          <div className="h-screen relative w-full bg-black flex flex-col items-center justify-center overflow-hidden rounded-md">
+          <div className="h-[700px] md:h-[900px] xl:h-[800px] relative w-full bg-black flex flex-col items-center justify-center overflow-hidden rounded-md">
               <div className="w-full absolute inset-0 h-screen">
                   <SparklesCore id="tsparticlesfullpage" background="transparent" minSize={0.6} maxSize={1.4} particleDensity={100} className="w-full h-full" particleColor="#FFFFFF"/>
               </div>
@@ -14,16 +14,16 @@ const Home = () => {
                   Výčetka
               </h1>
               <div className="z-10 flex flex-col md:flex-row gap-10 pt-12">
-                  <Image className="outline-1 outline-white rounded-lg" width={200} height={200} alt="get it on google play" src="/googleplay.png"></Image>
+                  <Link href="/">
+                      <Image className="outline-1 outline-white rounded-lg" width={200} height={200} alt="get it on google play" src="/googleplay.png"></Image>
+                  </Link>
                   <Image className="opacity-45 outline-1 outline-white rounded-lg" width={200} height={200} alt="get it on google play" src="/appstore.png"></Image>
               </div>
           </div>
-          <div className="border-t border-t-gray-300 flex md:justify-evenly flex-row bg-black z-10 p-4 text-gray-300">
-              <p className="hidden md:flex text-sm md:text-lg py-2">&copy; 2024 Max Juškevič. Všechna práva vyhrazena.</p>
-              <Link href="/privacy">
-                  <button className="shadow-[0_0_0_3px_#000000_inset] px-6 py-2 bg-transparent border border-black dark:border-gray-300 dark:text-white text-black rounded-full font-bold transform hover:-translate-y-1 transition duration-400">
-                      Ochrana soukromí
-                  </button>
+          <div className="border-t border-t-gray-300 flex md:justify-evenly flex-col md:flex-row bg-black z-10 p-4 text-gray-300">
+              <p className="md:text-lg py-2 md:py-0">&copy; 2024 Max Juškevič. Všechna práva vyhrazena.</p>
+              <Link href="/privacy" className="underline md:text-lg">
+                  Ochrana soukromí
               </Link>
           </div>
       </>
